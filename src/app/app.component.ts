f5cc4fb8641d319323dcd5135fa33a8e4fd75b1a
Elias Valenzuela <elias.ariel.valenzuela@gmail.com>
2024-08-13 00:04:05 -0300

    Styles added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxSpinnerModule } from "ngx-spinner";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxSpinnerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'employeApp';
+  
 }
